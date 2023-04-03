88ece19b3271447a4fd9ecefa55ec8652e11f9ec
MikaV-v <mcvdoc1@gmail.com>
2023-04-03 14:24:27 +0300

    commit 2

--- app/src/main/java/com/example/nikk0teen/presentation/fragments/MainFragment.kt
+++ app/src/main/java/com/example/nikk0teen/presentation/fragments/MainFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.nikk0teen.R
 
 class MainFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +13,6 @@ class MainFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 }
